8ae0f42afa5082e30475fdd26037204d9db0851a
Dinuka-Sandaruwan-Warnasooriya <dinukaswarnasooriya@gmail.com>
2021-08-15 05:27:35 +0530

    Added mvc to aplication

---
+++ e-shop/e-shop/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Dinuka";
+        }
+    }
+}
